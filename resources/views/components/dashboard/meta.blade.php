75701627786e91cadfa417c3fefe1c8ab7e038fb
dodi345 <dodisopandi89@gmail.com>
2024-10-12 23:04:10 +0700

    ajig lah

--- resources/views/components/dashboard/meta.blade.php
+++ resources/views/components/dashboard/meta.blade.php
@@ -1,21 +1,23 @@
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="./assets/img/favicon.png" />
-    <title>Soft UI Dashboard Tailwind</title>
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <!-- Nucleo Icons -->
-    <link href="{{ asset('vendor/build') }}/assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="{{ asset('vendor/build') }}/assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Popper -->
-    <script src="https://unpkg.com/@popperjs/core@2"></script>
-    <!-- Main Styling -->
-    <link href="{{ asset('vendor/build') }}/assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />
-    <!-- Nepcha Analytics (nepcha.com) -->
-    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+    <meta name="description" content="POS - Bootstrap Admin Template" />
+    <meta name="keywords"
+        content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern,  html5, responsive" />
+    <meta name="author" content="Dreamguys - Bootstrap Admin Template" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Dreams Pos admin template</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('vendor/inventory') }}/assets/img/favicon.jpg" />
+
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/css/animate.css" />
+
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/css/dataTables.bootstrap4.min.css" />
+
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/plugins/fontawesome/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/plugins/fontawesome/css/all.min.css" />
+
+    <link rel="stylesheet" href="{{ asset('vendor/inventory') }}/assets/css/style.css" />
 </head>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,40 +1,39 @@
-<!--
-=========================================================
-* Soft UI Dashboard Tailwind - v1.0.5
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-tailwind
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
-<x-dashboard.meta />
+<x-dashboard.meta/>
+<body>
+    <div id="global-loader">
+        <div class="whirly-loader"></div>
+    </div>
 
-<body class="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500">
-    <x-dashboard.sidebar />
+    <div class="main-wrapper">
+        <x-dashboard.header>
 
-    <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
-        <x-dashboard.navbar />
+        <x-dashboard.sidebar>
 
-        {{ $slot }}
-    </main>
+        <div class="page-wrapper">
+            <div class="content">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
 
-    <x-dashboard.footer />
+    <script src="{{ asset('vendor/inventory') }}/assets/js/jquery-3.6.0.min.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/js/feather.min.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/js/jquery.slimscroll.min.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('vendor/inventory') }}/assets/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/plugins/apexchart/apexcharts.min.js"></script>
+    <script src="{{ asset('vendor/inventory') }}/assets/plugins/apexchart/chart-data.js"></script>
+
+    <script src="{{ asset('vendor/inventory') }}/assets/js/script.js"></script>
 </body>
-<!-- plugin for charts  -->
-<script src="./assets/js/plugins/chartjs.min.js" async></script>
-<!-- plugin for scrollbar  -->
-<script src="./assets/js/plugins/perfect-scrollbar.min.js" async></script>
-<!-- github button -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- main script file  -->
-<script src="./assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
 
 </html>
